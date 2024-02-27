fa62f1d4ebf6128864cbc5137ddb2723ac338ac2
brueqlv <gundarens@gmail.com>
2024-02-27 09:39:46 +0200

    Fixed null checks in partial view

--- src/Clean.RGP.Web/Controllers/RGPController.cs
+++ src/Clean.RGP.Web/Controllers/RGPController.cs
@@ -1,5 +1,10 @@
-﻿using Clean.RGP.UseCases.Contributors.List;
+﻿using Clean.RGP.Core.PersonAggregate;
+using Clean.RGP.UseCases.Contributors.List;
+using Clean.RGP.UseCases.People.Create;
+using Clean.RGP.UseCases.People.Delete;
+using Clean.RGP.UseCases.People.Get;
 using Clean.RGP.UseCases.People.List;
+using Clean.RGP.UseCases.People.Update;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +21,61 @@ public class RGPController : Controller
   public IActionResult Index()
   {
     var peopleList = _mediator.Send(new GetAllPeopleListQuery());
+
     return View(peopleList.Result.Value);
+  }
+
+  public async Task<ActionResult> Details(int id)
+  {
+    Person person = await _mediator.Send(new GetPersonByIdQuery(id));
+
+    return View(person);
+  }
+
+  public ActionResult Create()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public async Task<ActionResult> Create(Person model)
+  {
+    if (!ModelState.IsValid)
+    {
+      return View(model);
+    }
+
+    await _mediator.Send(new AddNewPersonCommand(model));
+
+    return RedirectToAction("Index");
+  }
+
+  public async Task<IActionResult> Edit(int id)
+  {
+    Person person = await _mediator.Send(new GetPersonByIdQuery(id));
+
+    return View(person);
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public async Task<ActionResult> Edit(int id, Person person)
+  {
+    if (!ModelState.IsValid)
+    {
+      return View(person);
+    }
+
+    await _mediator.Send(new EditPersonByIdCommand(id, person));
+
+    return RedirectToAction("Index");
+  }
+
+  public async Task<ActionResult> Delete(int id)
+  {
+    await _mediator.Send(new DeletePersonByIdCommand(id));
+
+    return RedirectToAction("Index");
   }
 }
